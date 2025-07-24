a1fa6aae8c7ecc2e61d414158348130317224bca
jihostudy <cale.jiho@gmail.com>
2025-07-24 20:40:05 +0900

    fix: AI 채팅 보내기 테스트 시나리오 수정

--- stresstest/app/ai/services.js
+++ stresstest/app/ai/services.js
@@ -1,9 +1,7 @@
 const generateAiResponse = async (page, aiMention) => {
-  const messageInput = await page.getByPlaceholder('메시지를 입력하세요... (@를 입력하여 멘션,');
+  const messageInput = await page.getByPlaceholder('메시지를 입력하세요... (@를 입력하여 멘션, Shift + Enter로 줄바꿈)');
   await messageInput.fill(`${aiMention} hi`);
-
-  const sendButton = await page.getByRole('button', { name: '메시지 보내기' });
-  await sendButton.click();
+  await page.getByRole('button', { name: '메시지 보내기' }).click();
 
   console.info('AI response generated.');
   await page.waitForTimeout(5000);
